000407acf80f1860039f4fbf79c6b75a6b4c54f2
Ayush Nandi <110190468+ayushnandi@users.noreply.github.com>
2024-02-04 09:56:58 +0530

    Done with setup and added colour highlights

---
+++ Portfolio/src/components/Home.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+export default function Home() {
+    const name = "Ayush Nandi"
+  return (
+    <div>
+      <h1> Hello Ayush Nandi ! </h1>
+    </div>
+  )
+}
--- Portfolio/tailwind.config.js
+++ Portfolio/tailwind.config.js
@@ -2,8 +2,17 @@
 export default {
   content: ["*"],
   theme: {
-    extend: {},
+    extend: {
+      colors:{
+        "bgHome": "#0A0A0A",
+        "sk1": "#EEE5E0",
+        "sk2": "#BAACA3",
+        "sk3": "#D9C7BD",
+        "fadeBlack": "#4A4B4F",
+      }
+    },
   },
+  // Done with setup and added colour highlights
   plugins: [],
 }
 
